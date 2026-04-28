07a48445814101f60c689375c180f1b522dff923
chener <2024962075@qq.com>
2026-04-28 12:08:18 +0800

    写入 Embedding 模型配置

--- backend/src/models/model.py
+++ backend/src/models/model.py
@@ -1,10 +1,17 @@
 from langchain.chat_models import init_chat_model
+from langchain_openai import OpenAIEmbeddings
 
 from src.config import settings
 
 llm = init_chat_model(
     model="qwen-flash",
     model_provider="openai",
+    api_key=settings.DASHSCOPE_API_KEY,
+    base_url=settings.DASHSCOPE_BASE_URL,
+)
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-v1",
     api_key=settings.DASHSCOPE_API_KEY,
     base_url=settings.DASHSCOPE_BASE_URL,
 )
